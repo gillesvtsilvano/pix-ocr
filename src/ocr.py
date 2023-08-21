84ab9d5dbf0c8d4ff5387094104cff3043e52d3a
Gilles Silvano <gillessilvano@rn.gov.br>
2023-08-21 09:30:02 -0300

    Removendo tesseract-ocr

--- src/ocr.py
+++ src/ocr.py
@@ -6,8 +6,9 @@ https://stackoverflow.com/questions/47995993/pytesseract-results-different-from-
 
 class OCR:
     __TESSERACT_PARAMS = '--oem 3 --psm 6'
-
+    __TESSERACT_LANG='por'
+    
     @classmethod
     def image_to_text(cls, img):
-        text = pytesseract.image_to_string(img, config=cls.__TESSERACT_PARAMS)
+        text = pytesseract.image_to_string(img, lang=cls.__TESSERACT_LANG, config=cls.__TESSERACT_PARAMS)
         return text
